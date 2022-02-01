31dfc087264f5378bf82ad02537c82869c43a976
Gegardus <stgegard@gmail.com>
2022-02-01 12:25:41 +0400

    Implement refresh btn, add placeholder for msg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+// Implement the "Refresh" button (receiving data from the API and parsing the JSON)
+
+const refreshBtn = document.querySelector('.refresh-btn');
+
+refreshBtn.addEventListener('click', () => {
+    const list = document.querySelector('.score-list');
+
+    while(list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+
+    const msg = document.querySelector('.msg');
+
+    msg.innerHTML = '';
+});
